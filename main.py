a6ad3d444d91916caa03da43683620c784f0cf29
mylaBerozkina <77445070+mylaBerozkina@users.noreply.github.com>
2023-08-17 12:29:31 +0300

    add beads_counter function to the main.py

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from get_text_from_pdf import get_text_from_pdf
 from create_dictionary import create_dictionary
 
-text_page = get_text_from_pdf('Olha 1.pdf')
+def beads_counter(*pdf_files):
+	result_list = []
+	for i in pdf_files:
+		text_page = get_text_from_pdf(i)
+		dictionary = create_dictionary(text_page)
+		result_list.append(dictionary)
+	result_dict = {}
+	for d in result_list:
+		for key in d:
+			if key in result_dict:
+				result_dict[key] = -(-(result_dict[key] + d[key])//80)
+			else:
+				result_dict[key] = -(-d[key]//80)
 
-dictionary = create_dictionary(text_page)
+	return result_dict
 
+dictionary = beads_counter("Olha 1.pdf", "i_love_sea.pdf", "Kosiv.pdf")
 print(dictionary)
